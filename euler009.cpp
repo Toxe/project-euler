9c15df6dd3491cd2590ba5c0f6fe92f675c04430
Toxe <toxe@toxe.net>
2019-06-28 18:09:55 +0200

    Problem 9

---
+++ euler009.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int euler009()
+{
+    for (int a = 1; a <= (1000/3); ++a) {
+        for (int b = a + 1; b <= (999 - a) / 2; ++b) {
+            int c = 1000 - (a + b);
+
+            if ((a * a) + (b * b) == (c * c))
+                return a * b * c;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    std::cout << euler009() << std::endl;
+}
