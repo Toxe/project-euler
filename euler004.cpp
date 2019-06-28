5a7b6072ddc72fe94c0311cd821d1b014c126c41
Toxe <toxe@toxe.net>
2019-06-28 06:37:48 +0200

    Problem 4

---
+++ euler004.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+template <typename T>
+bool is_palindrome_number(const T number)
+{
+    const std::string s{std::to_string(number)};
+    auto right = s.cend() - 1;
+
+    for (auto left = s.cbegin(); left < right; ++left, --right)
+        if (*left != *right)
+            return false;
+
+    return true;
+}
+
+int euler004()
+{
+    int max = 0;
+
+    for (int i = 999; i >= 100; --i) {
+        for (int j = i; j >= 100; --j) {
+            const int n = i * j;
+
+            if (n > max)
+                if (is_palindrome_number(n))
+                    max = n;
+        }
+    }
+
+    return max;
+}
+
+int main()
+{
+    std::cout << euler004() << std::endl;
+}
