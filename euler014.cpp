47cb8f050ac027547db3bbae611a646070533458
Toxe <toxe@toxe.net>
2019-06-30 16:33:39 +0200

    Problem 14

---
+++ euler014.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+long long next_in_collatz_sequence(long long n)
+{
+    if (n <= 1)
+        return 1;
+
+    if (n % 2)
+        return 3 * n + 1;
+
+    return n / 2;
+}
+
+int calc_collatz_sequence_length(long long n)
+{
+    int sequence_length = 1;
+
+    while (n > 1) {
+        n = next_in_collatz_sequence(n);
+        ++sequence_length;
+    }
+
+    return sequence_length;
+}
+
+int euler014()
+{
+    int longest_sequence = 0;
+    int longest_sequence_number = 0;
+
+    for (int i = 1; i < 1000000; ++i) {
+        int length = calc_collatz_sequence_length(i);
+
+        if (length > longest_sequence) {
+            longest_sequence = length;
+            longest_sequence_number = i;
+        }
+    }
+
+    return longest_sequence_number;
+}
+
+int main()
+{
+    std::cout << euler014() << std::endl;
+}
